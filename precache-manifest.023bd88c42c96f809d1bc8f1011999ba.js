fc408e6da746e1f7bf38b9f718ace22558666fc3
Milosz Zajonc <milosz.zajonc@gmail.com>
2019-10-29 16:01:48 +0100

    Updates

---
+++ precache-manifest.023bd88c42c96f809d1bc8f1011999ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb783bf928a7abe5cb55a8cea5bf3e46",
+    "url": "/charcounter/index.html"
+  },
+  {
+    "revision": "d67fe79278788048a4f8",
+    "url": "/charcounter/static/css/main.82774bb6.chunk.css"
+  },
+  {
+    "revision": "2675c4418dca32192fac",
+    "url": "/charcounter/static/js/2.f0ca6cc1.chunk.js"
+  },
+  {
+    "revision": "d67fe79278788048a4f8",
+    "url": "/charcounter/static/js/main.90f1b9cb.chunk.js"
+  },
+  {
+    "revision": "ef021d8c9aa428d47ab9",
+    "url": "/charcounter/static/js/runtime-main.f44d0761.js"
+  },
+  {
+    "revision": "33545ec6ffda90e786dea627ce363688",
+    "url": "/charcounter/static/media/memphis-mini.33545ec6.png"
+  }
+]);
